dc8550cdc381becaa68124a644134958c3c06626
SueArt <1595569006@qq.com>
2019-04-06 17:06:54 +0800

    Site updated: 2019-04-06 17:06:50

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -2,12 +2,12 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/images/no.jpeg");
+        $('[rel="icon"]').attr('href', "https://www.bilibili.com/favicon.ico");
         document.title = '页面崩溃啦qwq~';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "/images/ok.jpeg");
+        $('[rel="icon"]').attr('href', "https://www.bilibili.com/favicon.ico");
         document.title = '噫awa又好了~'; //+ OriginTitle
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
