10e06ac2e5ce5b7a63ee03c6c9a4daf3ca04c8a3
SueArt <1595569006@qq.com>
2019-05-18 10:56:22 +0800

    Site updated: 2019-05-18 10:56:14

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -2,15 +2,17 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "https://www.bilibili.com/favicon.ico");
+        $('[rel="icon"]').attr('href', "https://s2.ax1x.com/2019/05/18/EOEB4g.png");
         document.title = '页面崩溃啦qwq~';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "https://www.bilibili.com/favicon.ico");
+        $('[rel="icon"]').attr('href', "https://s2.ax1x.com/2019/05/18/EOEB4g.png");
         document.title = '噫awa又好了~'; //+ OriginTitle
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
+
+        //B站图标 https://www.bilibili.com/favicon.ico
     }
 });
